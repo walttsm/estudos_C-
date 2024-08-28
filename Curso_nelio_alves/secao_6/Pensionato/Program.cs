30454d2f2cc3f98e1c810dc4e2135b71f026dc60
Walter Marinho <waltmarinho@gmail.com>
2024-08-28 15:13:28 -0300

    Exercício de pensionato

---
+++ Curso_nelio_alves/secao_6/Pensionato/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Tenant[] rooms = new Tenant[10];
+
+        Console.Write("How many rooms will be rented? ");
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine();
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Room: ");
+            int room = int.Parse(Console.ReadLine());
+
+            rooms[room] = new Tenant(name, email);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Busy rooms:");
+        for (int i = 0; i < 10; i++)
+        {
+            if (rooms[i] != null)
+            {
+                Console.WriteLine(i + ": " + rooms[i]);
+            }
+        }
+    }
+}
---
+++ Curso_nelio_alves/secao_6/Pensionato/Tenant.cs
@@ -0,0 +1,17 @@
+class Tenant
+{
+
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public Tenant(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    public override string ToString()
+    {
+        return Name + ", " + Email;
+    }
+}
